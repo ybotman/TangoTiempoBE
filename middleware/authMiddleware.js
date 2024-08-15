e36cc1d8b1cac4119df7409ffa32d896891e4926
Tural Novruzov <tural.nt.1@gmail.com>
2024-08-15 19:18:44 -0400

    Add firebase auth middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+// src/middleware/authMiddleware.js
+const admin = require('../lib/firebaseAdmin');
+
+const authenticateToken = async (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1]; // Bearer <token>
+
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    console.error('Error verifying token:', error);
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+module.exports = authenticateToken;
