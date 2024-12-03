1a6d02182aabfe30bbd57a76a8091d040a655d21
YbotMan <toby.balsley+YbotMan@gmail.com>
2024-12-03 17:26:20 -0500

    BE RO APPLY working.

--- models/organizers.js
+++ models/organizers.js
@@ -7,9 +7,9 @@ const organizerSchema = new mongoose.Schema({
     required: true,
   },
   firebaseUserId: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
-  fullName: { type: String, required: true },
-  shortName: { type: String, required: true },
+  name: { type: String, required: true, default:'New Organzier'  },
+  fullName: { type: String, required: true, default : 'New Organzier' },
+  shortName: { type: String, required: true, default: 'CHANGE'},
   description: { type: String },
   publicContactInfo: {
     phone: { type: String },
